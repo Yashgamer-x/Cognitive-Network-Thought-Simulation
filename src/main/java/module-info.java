e996db2e8776d6add5f7568625a4e07eea790ec2
yashgamerx <varoonnoob@gmail.com>
2025-07-17 22:19:55 -0400

    Normal Updates

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,6 +4,12 @@ module com.yashgamerx.cognitive_thought_network_simulation {
     requires static lombok;
 
 
-    opens com.yashgamerx.cognitive_thought_network_simulation to javafx.fxml;
+    opens com.yashgamerx.cognitive_thought_network_simulation to javafx.fxml, javafx.graphics;
     exports com.yashgamerx.cognitive_thought_network_simulation;
+    exports com.yashgamerx.cognitive_thought_network_simulation.individuals;
+    opens com.yashgamerx.cognitive_thought_network_simulation.individuals to javafx.fxml, javafx.graphics;
+    exports com.yashgamerx.cognitive_thought_network_simulation.storage;
+    opens com.yashgamerx.cognitive_thought_network_simulation.storage to javafx.fxml, javafx.graphics;
+    exports com.yashgamerx.cognitive_thought_network_simulation.manager;
+    opens com.yashgamerx.cognitive_thought_network_simulation.manager to javafx.fxml, javafx.graphics;
 }
